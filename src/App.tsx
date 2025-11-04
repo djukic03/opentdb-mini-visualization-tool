504a5f0f4ab619bb8edeb6ca456106f0add2837b
djukic03 <djukic.nemanja003@gmail.com>
2025-11-04 01:25:35 +0100

    BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Questions from './pages/Questions'
 import Charts from './pages/Charts'
@@ -8,13 +8,13 @@ import { QuestionsProvider } from './contexts/questions-context'
 function App() {
   return (
     <QuestionsProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/play-trivia" element={<Questions/>} />
           <Route path="/charts" element={<Charts/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </QuestionsProvider>
   )
 }
